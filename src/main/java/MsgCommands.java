21bdc3e0eba06f50de77f0fcb5923d6f6514405f
NorbertasL <nlapenas@gmail.com>
2021-09-15 23:00:15 +0100

    CCommand frameworks

--- src/main/java/MsgCommands.java
+++ src/main/java/MsgCommands.java
@@ -1,17 +1,32 @@
-import net.dv8tion.jda.api.entities.VoiceChannel;
-import net.dv8tion.jda.api.events.channel.voice.VoiceChannelCreateEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 
 public class MsgCommands extends ListenerAdapter {
-    
-
+    private String botChannelName;
+    public MsgCommands(String botChannelName){
+        this.botChannelName = botChannelName;
+    }
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
-        System.out.println("Somethig happened");
+        if(!event.getChannel().getName().equalsIgnoreCase(botChannelName)){
+            System.out.println("Uninteresting channel");
+            return;
+        }
+        String msg = event.getMessage().getContentRaw();
+        if(msg.charAt(0)!='!'){
+            System.out.println("Not a command");
+        }
+        String[] msgData = msg.substring(1).split(" ");
+        if(msgData[0].equalsIgnoreCase("Event")){
+            //Create new event channel
+            event.getGuild().createTextChannel(msgData[1], event.getGuild().getCategories()
+                    .stream()
+                    .filter(x -> x.getName().equalsIgnoreCase("Events"))
+                    .findFirst().get()).queue();
+        }
     }
+
 
 
 }
