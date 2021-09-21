c8c4d748f5665458e762262eee336cafb233664f
NorbertasL <nlapenas@gmail.com>
2021-09-21 20:43:03 +0100

    More Variables

--- src/main/java/data/GlobalConstants.java
+++ src/main/java/data/GlobalConstants.java
@@ -1,18 +1,20 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+package data;
 
 //Using this until I set up a DB and make these more dynamic.
 
 public enum GlobalConstants {
     EVENT_CATEGORY("Events"),
-    PRIVATE_VC_CATEGORY("Private Voice"),
+    PRIVATE_VC_CATEGORY("Temp VC"),
     PRIVATE_VC_NAME("Create VC"),
-    DYNAMIC_VC_CATEGORY("Voice");
+    DYNAMIC_VC_CATEGORY("Voice"),
+    BOT_CHANNEL_NAME("bot");
 
     private final String name;
     GlobalConstants(String name){
         this.name = name;
+    }
+    public String getName(){
+        return this.name;
     }
 
 
---
+++ src/main/java/data/Variables.java
@@ -0,0 +1,50 @@
+package data;
+
+import net.dv8tion.jda.api.entities.Guild;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Variables {
+    private static List<Variables> variables = new ArrayList<>();
+    private String guildID;
+    public String getGuildID(){
+        return this.guildID;
+    }
+    public Variables(Guild guild){
+        this.guildID = guild.getId();
+        System.out.println("Making new guild variables class for:"+guild.getName());
+        System.out.println("ID is:"+this.guildID);
+        variables.add(this);
+        System.out.println("New settings.Variables list is:");
+        variables.stream().forEach(i -> System.out.println(i.getGuildID()));
+    }
+    public static Variables getVariables(Guild guild){
+        for(Variables v: variables){
+            if(v.getGuildID().equalsIgnoreCase(guild.getId())){
+                System.out.println("Pre-Existing Variables found for:"+v.guildID);
+                return v;
+            }
+        }
+        System.out.println("Guild settings.Variables could not be found");
+        Variables v = new Variables(guild);
+        return v;
+    }
+
+
+    public String getPrivateVcCategoryName(){
+        return GlobalConstants.PRIVATE_VC_CATEGORY.getName();
+    }
+    public String getPrivateChannelName(){
+        return GlobalConstants.PRIVATE_VC_NAME.getName();
+    }
+    public String getDynamicVcCategoryName() {
+        return GlobalConstants.DYNAMIC_VC_CATEGORY.getName();
+    }
+    public String getBotChannelName() {
+        return GlobalConstants.BOT_CHANNEL_NAME.getName();
+    }
+    public String getEventCategoryName() {
+        return GlobalConstants.EVENT_CATEGORY.getName();
+    }
+}
