c64b4ff1ea75bcdac93a8edae243d4299316d6d3
NorbertasL <nlapenas@gmail.com>
2021-09-22 16:19:44 +0100

    spelling fix

--- src/main/java/tasks/VCTasks.java
+++ src/main/java/tasks/VCTasks.java
@@ -22,7 +22,7 @@ public class VCTasks {
                 && vc.getName().equalsIgnoreCase(variables.getPrivateChannelName())){
             System.out.println("Making new private voice");
             String userName = event.getMember().getNickname()== null ? event.getMember().getUser().getName() : event.getMember().getNickname();
-            event.getGuild().createVoiceChannel(userName+"s Black Hole!", event.getChannelJoined().getParent()).queue();
+            event.getGuild().createVoiceChannel(userName.endsWith("s")?userName+"'": userName+"'s"+" Black Hole!", event.getChannelJoined().getParent()).queue();
             //Dynamic channel generator
         }else if(vc.getParent().getName().equalsIgnoreCase(variables.getDynamicVcCategoryName())){
             if(vc.getName().equalsIgnoreCase("AFK")) return;//Ignoring AFK channel
