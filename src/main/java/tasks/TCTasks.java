d3ad4ccd28fd99de0bf07d0f8df0ce39ece6984e
NorbertasL <nlapenas@gmail.com>
2021-09-24 22:27:21 +0100

    Exception suppression

--- src/main/java/tasks/TCTasks.java
+++ src/main/java/tasks/TCTasks.java
@@ -1,10 +1,16 @@
 package tasks;
 
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.exceptions.ContextException;
 
 public class TCTasks {
     public static void sendMessage(TextChannel textChannel, String msg){
-        System.out.println("Sending msg:"+msg);
-        textChannel.sendMessage(msg).queue();
+        try{
+            System.out.println("Sending msg:"+msg);
+            textChannel.sendMessage(msg).queue();
+        }catch (Exception e){
+            System.out.println("sendMessage error:"+e);
+        }
+
     }
 }
