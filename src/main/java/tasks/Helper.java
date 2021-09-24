8b7281443549d728038cc43a03ccef14abd4d033
NorbertasL <nlapenas@gmail.com>
2021-09-24 21:07:35 +0100

    A2 new Helper methods

--- src/main/java/tasks/Helper.java
+++ src/main/java/tasks/Helper.java
@@ -3,6 +3,9 @@ package tasks;
 import data.Variables;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.nio.channels.Channel;
 
 public class Helper {
     public static boolean hasRank(Member user, String...ranks){
@@ -12,6 +15,29 @@ public class Helper {
                 if(role.getName().equalsIgnoreCase(r)) return true;
             }
 
+        }
+        return false;
+    }
+
+    public static boolean hasCategory(TextChannel channel, String[] listenCategories) {
+        if(listenCategories == null){
+            return true;//will always be in right category :D
+        }
+        if(channel.getParent() == null){
+            return false;//No parent = no category so false
+        }
+        for(String c: listenCategories){
+            if(c.equalsIgnoreCase(channel.getParent().getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean channelInList(TextChannel channel, String[] channels){
+        for(String c : channels){
+            if(channel.getName().equalsIgnoreCase(c)){
+                return true;
+            }
         }
         return false;
     }
