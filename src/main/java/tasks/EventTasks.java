fd374f8edb46c8fc49e952854d701afbbbdd7772
NorbertasL <nlapenas@gmail.com>
2021-09-24 21:08:34 +0100

    Rank/channel check no longer performed by tasks

--- src/main/java/tasks/EventTasks.java
+++ src/main/java/tasks/EventTasks.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import data.GlobalConstants;
 import data.Variables;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
@@ -7,14 +8,9 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class EventTasks {
-    private static String role = "Event Manager";
+
     public static void makeNewEvent(Message eventMessage, String commandInfo){
         String callerName = eventMessage.getMember().getEffectiveName();
-        if(!Helper.hasRank(eventMessage.getMember(),role)){
-            System.out.println();
-            TCTasks.sendMessage(eventMessage.getTextChannel(), String.format("Sorry %s, you need \"%s\" role to make new events", callerName, role));
-            return;
-        }
         String [] cmds = commandInfo.split("[|]");
         //System.out.println(cmds[0]);
         Guild guild = eventMessage.getGuild();
@@ -23,25 +19,22 @@ public class EventTasks {
         variables.addEvent(id, eventMessage.getMember(), cmds);
         guild.createTextChannel(id, guild.getCategories()
                 .stream()
-                .filter(x -> x.getName().equalsIgnoreCase(variables.getEventCategoryName()))
+                .filter(x -> x.getName().equalsIgnoreCase(GlobalConstants.EVENT_CATEGORY[0]))
                 .findFirst().get()).queue();
         System.out.println("New Event queued with id:"+id+ " by "+callerName);
     }
 
     public static void closeEvent(Member caller, TextChannel channel, String commandInfo) {
-        //TODO handle event close
+
+
+
+        //TODO handle event archiving
     }
-    public static void deleteEvent(Member caller, TextChannel channel, String commandInfo) {
-        Variables variables = Variables.getVariables(channel.getGuild());
-        if(channel.getParent() == null || !channel.getParent().getName().equalsIgnoreCase(variables.getEventCategoryName())){
-            return;
-        }
-        String callerName = caller.getEffectiveName();
-        if(!Helper.hasRank(caller,role)){
-            System.out.println();
-            TCTasks.sendMessage(channel, String.format("Sorry %s, you need \"%s\" role to delete events", callerName, role));
-            return;
-        }
+    public static void deleteEvent(Message eventMessage, String commandInfo) {
+        String callerName = eventMessage.getMember().getEffectiveName();
+
+        TextChannel channel = eventMessage.getTextChannel();
+
         System.out.println("Deleting channel:"+channel.getName()+" as requested by:"+callerName);
         channel.delete().queue();
 
